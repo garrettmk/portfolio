952289ae0c1471634f2d219384862633c0bb38c3
Garrett Myrick <garrettmyrick@gmail.com>
2024-01-19 18:53:22 +0000

    first run of Timeline component

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -9,97 +9,101 @@ export default function Experience() {
       </Hero>
       <div className="max-w-xl py-8 mx-auto">
         <Timeline>
-          <Timeline.Item>
-            <Timeline.Item.Dot open>
-              Present
-            </Timeline.Item.Dot>
-            <Timeline.Item.Duration emphasis>
-              <Timeline.Item.Title>
-                Henry Schein One
-              </Timeline.Item.Title>
-              <Timeline.Item.Subtitle>
-                Software Engineer
-              </Timeline.Item.Subtitle>
-              <Timeline.Item.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
-                aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
-              </Timeline.Item.Body>
-            </Timeline.Item.Duration>
-            <Timeline.Item.Dot>
-              April 2020
-            </Timeline.Item.Dot>
-          </Timeline.Item>
+          <Timeline.Dot open>
+            Present
+          </Timeline.Dot>
 
-          <Timeline.Item>
-            <Timeline.Item.Duration emphasis>
-              <Timeline.Item.Title>
-                Square Practice
-              </Timeline.Item.Title>
-              <Timeline.Item.Subtitle>
-                Software Engineer
-              </Timeline.Item.Subtitle>
-              <Timeline.Item.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
-                aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
-              </Timeline.Item.Body>
-            </Timeline.Item.Duration>
-            <Timeline.Item.Dot>
-              April 2018
-            </Timeline.Item.Dot>
-          </Timeline.Item>
+          <Timeline.Duration emphasis>
+            <Timeline.Duration.Title>
+              Henry Schein One
+            </Timeline.Duration.Title>
+            <Timeline.Duration.Subtitle>
+              Software Engineer
+            </Timeline.Duration.Subtitle>
+            <Timeline.Duration.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
+              aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
+            </Timeline.Duration.Body>
+          </Timeline.Duration>
           
-          <Timeline.Item>
-            <Timeline.Item.Duration quiet>
-              <Timeline.Item.Body quiet>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
-                aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
-              </Timeline.Item.Body>
-            </Timeline.Item.Duration>
-            <Timeline.Item.Dot>
-              June 2015
-            </Timeline.Item.Dot>
-          </Timeline.Item>
+          <Timeline.Dot>
+            April 2020
+          </Timeline.Dot>
 
-          <Timeline.Item>
-            <Timeline.Item.Duration emphasis>
-              <Timeline.Item.Title>
-                Efinancial
-              </Timeline.Item.Title>
-              <Timeline.Item.Subtitle>
-                Life Insurance Agent
-              </Timeline.Item.Subtitle>
-              <Timeline.Item.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
-                aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
-              </Timeline.Item.Body>
-            </Timeline.Item.Duration>
-            <Timeline.Item.Dot>
-              April 2018
-            </Timeline.Item.Dot>
-          </Timeline.Item>
+          <Timeline.Duration emphasis>
+            <Timeline.Duration.Title>
+              Square Practice
+            </Timeline.Duration.Title>
+            <Timeline.Duration.Subtitle>
+              Software Engineer
+            </Timeline.Duration.Subtitle>
+            <Timeline.Duration.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
+              aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
+            </Timeline.Duration.Body>
+            <Timeline.Duration.Subtitle className="mt-0 group-hover/timeline-item:mt-8">
+              Technical Support Specialist
+            </Timeline.Duration.Subtitle>
+            <Timeline.Duration.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
+              aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
+            </Timeline.Duration.Body>
+          </Timeline.Duration>
+          
+          <Timeline.Dot>
+            April 2018
+          </Timeline.Dot>
+          
+          <Timeline.Duration quiet>
+            <Timeline.Duration.Body quiet>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
+              aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
+            </Timeline.Duration.Body>
+          </Timeline.Duration>
 
-          <Timeline.Item>
-            <Timeline.Item.Duration emphasis>
-              <Timeline.Item.Title>
-                DataSphere, LLC
-              </Timeline.Item.Title>
-              <Timeline.Item.Subtitle>
-                Sales Associate
-              </Timeline.Item.Subtitle>
-              <Timeline.Item.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
-                aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
-              </Timeline.Item.Body>
-            </Timeline.Item.Duration>
-            <Timeline.Item.Dot>
-              April 2018
-            </Timeline.Item.Dot>
-          </Timeline.Item>
+          <Timeline.Dot>
+            June 2015
+          </Timeline.Dot>
+
+          <Timeline.Duration emphasis>
+            <Timeline.Duration.Title>
+              Efinancial
+            </Timeline.Duration.Title>
+            <Timeline.Duration.Subtitle>
+              Life Insurance Agent
+            </Timeline.Duration.Subtitle>
+            <Timeline.Duration.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
+              aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
+            </Timeline.Duration.Body>
+          </Timeline.Duration>
+          
+          <Timeline.Dot>
+            April 2018
+          </Timeline.Dot>
+
+          <Timeline.Duration emphasis>
+            <Timeline.Duration.Title>
+              DataSphere, LLC
+            </Timeline.Duration.Title>
+            <Timeline.Duration.Subtitle>
+              Sales Associate
+            </Timeline.Duration.Subtitle>
+            <Timeline.Duration.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              pulvinar, nunc vel tempor aliquam, massa sapien aliquet velit, eu
+              aliquam diam risus ac nunc. Donec tempor, nulla vitae molestie
+            </Timeline.Duration.Body>
+          </Timeline.Duration>
+          
+          <Timeline.Dot>
+            April 2018
+          </Timeline.Dot>
         </Timeline>
       </div>
     </>
--- src/app/experience/timeline.tsx
+++ src/app/experience/timeline.tsx
@@ -1,7 +1,175 @@
 import clsx from "clsx";
 import { HTMLAttributes } from "react";
-import TimelineItem, { type TimelineItemProps } from "./timeline-item";
 
+
+/**
+ * TimelineDot represents a point in time on the timeline. The dot can be
+ * either open or closed, and can contain any content (but is meant to contain
+ * text, like a date).
+ */
+
+export type TimelineDotProps = HTMLAttributes<HTMLLIElement> & {
+  open?: boolean;
+};
+
+export function TimelineDot(props: TimelineDotProps) {
+  const { open, className, children, ...liProps } = props;
+
+  return (
+    <li
+      className={clsx(
+        'timeline-dot',
+        'relative h-4 pl-6 text-sm font-bold text-slate-400',
+        'transition-all duration-300 ease-in-out',
+        
+        // When the timeline is hovered, fade all dots
+        'group-hover/timeline:opacity-50',
+
+        // If this dot is hovered, highlight it
+        'hover:!opacity-100',
+        
+        // If the next item is a duration and is hovered, highlight the current dot
+        'has-[+li.timeline-duration:hover]:!opacity-100',
+        
+        className
+      )}
+      {...liProps}
+    >
+      <div 
+        className={clsx(
+          'w-4 h-4 rounded-full',
+          open ? "border-2 border-green-300" : "bg-green-300",
+          // Center the dot on the left edge
+          "absolute top-1/2 left-0 translate-x-[-50%] translate-y-[-50%]",
+        )}
+      />
+        {children}
+    </li>
+  );
+}
+
+
+/**
+ * The title of a TimelineDuration
+ */
+export type TimelineDurationTitleProps = HTMLAttributes<HTMLHeadingElement>;
+
+export function TimelineDurationTitle(props: TimelineDurationTitleProps) {
+  const { className, children, ...headingProps } = props;
+
+  return (
+    <h3
+      className={clsx(
+        'text-lg font-bold text-slate-100',
+        className
+      )}
+      {...headingProps}
+    >
+      {children}
+    </h3>
+  );
+}
+
+
+/**
+ * The subtitle of a TimelineDuration
+ */
+export type TimelineDurationSubtitleProps = HTMLAttributes<HTMLHeadingElement>;
+
+export function TimelineDurationSubtitle(props: TimelineDurationSubtitleProps) {
+  const { className, children, ...headingProps } = props;
+
+  return (
+    <h4
+      className={clsx(
+        'text-sm font-bold text-slate-400',
+        'transition-all duration-300 ease-in-out',
+        className
+      )}
+      {...headingProps}
+    >
+      {children}
+    </h4>
+  );
+}
+
+/**
+ * The body text of a TimelineDuration. Collapsed and transparent by default,
+ * but expands and becomes opaque when the TimelineDuration is hovered.
+ */
+export type TimelineDurationBodyProps = HTMLAttributes<HTMLDivElement> & {
+  quiet?: boolean;
+};
+
+export function TimelineDurationBody(props: TimelineDurationBodyProps) {
+  const { quiet, className, children, ...divProps } = props;
+
+  return (
+    <div
+      className={clsx(
+        'text-md max-h-0 mt-0 opacity-0',
+        quiet ? 'text-slate-400' : 'text-slate-50',
+        'transition-all duration-300 ease-in-out',
+
+        // When the timeline duration is hovered, expand and become opaque
+        'group-hover/timeline-item:max-h-[100px] group-hover/timeline-item:opacity-100 group-hover/timeline-item:mt-4',
+        className
+      )}
+      {...divProps}
+    >
+      {children}
+    </div>
+  );
+}
+
+/**
+ * TimelineDuration is a duration of time on the timeline. It can be either
+ * a thin line (emphasis=false), a thick line (emphasis=true), or a dotted
+ * line (quiet=true).
+ */
+export type TimelineDurationProps = HTMLAttributes<HTMLLIElement> & {
+  emphasis?: boolean;
+  quiet?: boolean;
+};
+
+export function TimelineDuration(props: TimelineDurationProps) {
+  const { quiet, className, emphasis, children, ...divProps } = props;
+
+  return (
+    <li
+      className={clsx(
+        // Used as selectors by other components
+        'timeline-duration group/timeline-item',
+        'relative px-6 py-16 text-slate-400 border-green-300 rounded-r-xl',
+        emphasis ? 'border-l-4 translate-x-[-2px]' : 'border-l',
+        quiet ? 'border-dotted' : '',
+        'transition-all duration-300 ease-in-out',
+
+        // When the timeline is hovered, fade all durations
+        'group-hover/timeline:[filter:opacity(.5)]',
+
+        // If this duration is hovered, highlight it
+        'hover:![filter:opacity(1)] hover:py-24',
+        'hover:backdrop-brightness-[.85] hover:backdrop-blur-[2px]',
+        '[&+li.timeline-dot]:hover:!opacity-100',
+        
+        className
+      )}
+      {...divProps}
+    >
+      {children}
+    </li>
+  );
+}
+
+TimelineDuration.Title = TimelineDurationTitle;
+TimelineDuration.Subtitle = TimelineDurationSubtitle;
+TimelineDuration.Body = TimelineDurationBody;
+
+
+/**
+ * Timeline is a list of TimelineDots and TimelineDurations
+ */
 export type TimelineProps = HTMLAttributes<HTMLOListElement>
 
 export default function Timeline(props: TimelineProps) {
@@ -10,7 +178,7 @@ export default function Timeline(props: TimelineProps) {
   return (
     <ol 
       className={clsx(
-        'group',
+        'group/timeline',
         className
       )} 
       {...listProps}
@@ -20,5 +188,5 @@ export default function Timeline(props: TimelineProps) {
   );
 }
 
-Timeline.Item = TimelineItem;
-export { TimelineItem, type TimelineItemProps };
+Timeline.Dot = TimelineDot;
+Timeline.Duration = TimelineDuration;
