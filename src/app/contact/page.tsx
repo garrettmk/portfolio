bf862dbca9b7d4c3192d8603198826b197462bb7
Garrett Myrick <garrettmyrick@gmail.com>
2024-01-22 01:08:38 +0000

    added contact page stub

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,20 @@
+import Hero from '@/components/hero';
+
+export default function Contact() {
+  return (
+    <>
+      <Hero>
+        <Hero.Title>Talk to Me</Hero.Title>
+      </Hero>
+      <div className="max-w-3xl py-8 mx-auto">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
+          diam at ante vehicula feugiat id vitae urna. Pellentesque nec faucibus
+          urna. Suspendisse eros nunc, gravida in sapien in, ultrices
+          ullamcorper massa. Praesent et pellentesque enim, sed volutpat ante.
+          Quisque eleifend mollis odio, sit amet commodo mauris mattis ac.
+        </p>
+      </div>
+    </>
+  )
+}
