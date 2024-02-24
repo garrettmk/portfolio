e45c878a0815e36da96d731c23acdee5437d16c3
Garrett Myrick <garrettmyrick@gmail.com>
2024-02-24 09:14:30 -0800

    configure for deployment at base github page

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/portfolio',
   images: {
     unoptimized: true,
   },
